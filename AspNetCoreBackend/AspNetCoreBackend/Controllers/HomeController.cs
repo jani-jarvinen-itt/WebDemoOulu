e6eae566b996fa0eb23893ef6f8418acad16ef6a
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-02-22 15:39:10 +0200

    Lisätty Model-luokka autolle ja sitä varten yksinkertainen HTML-käyttöliittymä.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
@@ -49,5 +49,34 @@ namespace AspNetCoreBackend.Controllers
         {
             return View();
         }
+
+
+        public IActionResult Autot()
+        {
+            List<Auto> autot = new List<Auto>()
+            {
+                new Auto()
+                {
+                    Merkki = "Ford",
+                    Malli = "Focus",
+                    Huippunopeus = 190
+                },
+                new Auto()
+                {
+                    Merkki = "Toyota",
+                    Malli = "Avensis",
+                    Huippunopeus = 220
+                },
+                new Auto()
+                {
+                    Merkki = "Porsche",
+                    Malli = "911",
+                    Huippunopeus = 300
+                }
+            };
+
+            return View(autot);
+        }
+
     }
 }
