1f92d32b37ff5c4cd3ef6e952fdcec33e2b15bbb
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-03-06 11:01:13 +0200

    Lisätty API-kontrolleri asiakkaiden tietojen käsittelyyn.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/AsiakkaatApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/asiakkaat")]
+    [ApiController]
+    public class AsiakkaatApiController : ControllerBase
+    {
+
+        [Route("listaus")]
+        public List<Customers> Listaus()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> allCustomers = context.Customers.ToList();
+
+            return allCustomers;
+        }
+    }
+}
