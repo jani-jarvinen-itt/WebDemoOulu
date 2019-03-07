cb86a188e33a5df1284c2c42c137603e95306118
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-03-07 15:36:58 +0200

    Lisätty fetch-kutsu POST-metodia varten.

--- ReactFront/react-front/src/LähetysNappi.js
+++ ReactFront/react-front/src/LähetysNappi.js
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class LähetysNappi extends Component {
 
     lisääAsiakas() {
-        alert("Nappia klikattu!");
+
+        let data = 
+        {
+            "customerId": "REACT",
+            "companyName": "React-demon kautta!",
+            "contactName": "M. Möttönen",
+            "contactTitle": "Tirehtööri",
+            "address": "Web-devauspolku 1",
+            "city": "Oulu"
+        };
+
+        let url = "https://localhost:44358/api/asiakkaat/";
+        fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+
+        alert("Fetch-pyyntö lähetetty!");
     }
 
   render() {
